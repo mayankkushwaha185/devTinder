d2e9b407c14ae1c69d989ff10a8420e3ea5b8da2
Mayank Kushwaha <mayankKushwaha185@gmail.com>
2024-10-30 23:56:42 +0530

    init - databse

--- src/app.js
+++ src/app.js
@@ -1,30 +1,40 @@
 const express =require('express')
-
+const connectDB = require("./config/database")
 const app = express()
-const {userAuth, admin} = require("./middlewares/authMiddleware")
+const User = require("./Models/user")
 
-app.use("/admin", admin)
+app.post("/signup", async (req, res)=>{
+    const userObj1 = {
+        firstName: "Mayank",
+        lastName:"Kushwaha",
+        emailId:"maya@gamil.com",
+        password:"pasw123"
+    }
+    const userObj2={
+        firstName:"Manisha",
+        lastName:"Singh",
+        emailId:"manisha1999@gmail.com",
+        password:"Paww"
+    }
+    // Creating new user instance
+    const user = new User(userObj1)
+    const user2 = new User(userObj2)
 
-app.get("/admin/getData", (req,res)=>{
-    console.log("get data")
-    res.send("user data send")
-})
-app.get ("/admin/deleteUser", (req, res)=>{
-    console.log("")
-    res.send("User Deleted")
-})
-
-app.get("/user/data", userAuth, (req,res)=>{
-    res.send("user data send")
-})
-app.get("/user/login", (req,res)=>{
-    res.send("user LOgin")
+    try {
+           await user.save()
+           await user2.save()
+           res.send("user added succesfully")
+    }catch (err){
+        res.status(400).send("Error saving the user:" + err.message)
+    }
 })
 
-app.get("/user/delete", userAuth, (req,res)=>{
-    res.send("user data deleted")
-})
 
-app.listen(3000, ()=>{
-    console.log("Server is Running on server 0n port 3000")
+connectDB().then(()=>{
+    console.log("connected to Database")
+    app.listen(3000, ()=>{
+        console.log("Server is Running on server 0n port 3000")
+    })
+}).catch(err=>{
+    console.error("not connected to db ");
 })
--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDB = async ()=>{
- await mongoose.connect("mongodb+srv://mayankkushwaha185:lYOfMWt0OpAMLmNl@cluster0.htdv3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
+ await mongoose.connect("mongodb+srv://mayankkushwaha185:lYOfMWt0OpAMLmNl@cluster0.htdv3.mongodb.net/devTinder") 
 }
 
-connectDB().then(()=>{
-    console.log("conect to db")
-}).catch(err=>{
-    console.error("not conected ");
-})
+module.exports = connectDB;
